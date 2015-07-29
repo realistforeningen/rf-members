3b78a019a58b85012da376b4eb4657dc22036095
Magnus Holm <judofyr@gmail.com>
2015-07-29 12:08:44 +0200

    Add Session table:
    - This requires a completely new database.
    - Every membership is now created by a session.

--- rfmembers.py
+++ rfmembers.py
@@ -1,5 +1,6 @@
 from datetime import datetime, timedelta
-from flask import Flask, render_template, request, redirect, url_for, jsonify
+from functools import wraps
+from flask import Flask, render_template, request, redirect, url_for, jsonify, session, g, abort
 
 from flask.ext.script import Manager
 
@@ -10,6 +11,7 @@ from flask.ext.assets import Environment, Bundle
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['ASSETS_DEBUG'] = True # TODO: set this to false in production
+app.secret_key = "very secret"
 
 assets = Environment(app)
 
@@ -23,6 +25,12 @@ class Membership(db.Model):
     term = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
 
+    created_by = db.Column(db.Integer, db.ForeignKey('session.id'), nullable=False)
+    settled_by = db.Column(db.Integer, db.ForeignKey('session.id'), nullable=True)
+
+    created_session = db.relationship("Session", foreign_keys=[created_by], backref="created_memberships")
+    settled_session = db.relationship("Session", foreign_keys=[settled_by], backref="settled_memberships")
+
     def is_free(self):
         return self.price == 0
 
@@ -32,18 +40,110 @@ def parse_price(text):
     else:
         return 50
 
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.Text, nullable=False)
+    level = db.Column(db.Text, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
+    closed_at = db.Column(db.DateTime, nullable=True)
+
+    def can(self, action):
+        if self.level == 'Admin':
+            return True
+
+        if action == 'settlement':
+            return self.level == 'SM'
+
+        if action == 'memberships_list':
+            return self.level == 'SM'
+
+        if action == 'memberships_new':
+            return True
+
+        return False
+
+# TODO: Move these out to a config-file
+PASSWORDS = {
+    'Funk': 'funk',
+    'SM': 'sm',
+    'Admin': 'admin',
+}
+
+@app.before_request
+def before_request():
+    if 'session_id' in session:
+        sess = Session.query.get(session['session_id'])
+        setattr(g, 'sess', sess)
+    else:
+        setattr(g, 'sess', None)
+
+
+def logout():
+    session.pop('session_id')
+
+def requires(action):
+    def decorator(func):
+        @wraps(func)
+        def route():
+            if g.sess and g.sess.can(action):
+                return func()
+            else:
+                abort(404)
+        return route
+    return decorator
+
 @app.route('/')
 def index():
-    return redirect(url_for('memberships_new'))
+    if g.sess is None:
+        return render_template('index.html')
+    else:
+        return redirect(url_for('memberships_new'))
+
+@app.route('/sessions/new')
+def sessions_new():
+    level = request.args['level']
+    description = request.args['description']
+    return render_template('sessions/new.html', level=level, description=description)
+
+@app.route('/sessions/new', methods=['POST'])
+def sessions_create():
+    level = request.form["level"]
+    real_password = PASSWORDS[request.form["level"]]
+
+    if real_password != request.form["password"]:
+        # TODO: Show error
+        return sessions_new()
+
+    sess = Session(
+        level=level,
+        description=request.form.get("description", "Unknown"),
+    )
+    db.session.add(sess)
+    db.session.commit()
+    session["session_id"] = sess.id
+    return redirect(url_for('index'))
+
+@app.route('/sessions/delete', methods=['POST'])
+def sessions_destroy():
+    g.sess.closed_at = datetime.utcnow()
+    logout()
+    return redirect(url_for('index'))
 
 @app.route('/memberships/new')
+@requires('memberships_new')
 def memberships_new():
     membership = Membership(name=request.args.get('name', ''))
     return render_template('memberships/new.html', membership=membership)
 
 @app.route('/memberships/new', methods=['POST'])
+@requires('memberships_new')
 def memberships_create():
-    membership = Membership(name=request.form["name"], price=parse_price(request.form["price"]), term="V15")
+    membership = Membership(
+        name=request.form["name"],
+        price=parse_price(request.form["price"]),
+        term="V15", # TODO: Don't hard-code right here
+        created_by=g.sess.id
+    )
     if membership.name.strip() == '':
         return render_template('memberships/new.html', membership=membership)
 
@@ -80,6 +180,7 @@ def memberships_diff_formdate():
             memberships_added=memberships_added, from_date=from_date, cost=cost)
 
 @app.route('/memberships')
+@requires('memberships_list')
 def memberships_list():
     memberships = Membership.query.all()
     return render_template('memberships/list.html', memberships=memberships)
